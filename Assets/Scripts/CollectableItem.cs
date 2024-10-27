b0215b634c84b327f28e4d6fd51783d691f3f78c
CharlesMakesGames <cfisher@unomaha.edu>
2024-10-26 20:27:18 -0500

    Generalization of Collectables
    Inroads to more generalized collectables beyond mushrooms

--- Assets/Scripts/CollectableItem.cs
+++ Assets/Scripts/CollectableItem.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class CollectableItem : MonoBehaviour
 {
+    //player enters trigger area of item
+    //player pressed "pickup" button
+    //item is added to player inventory
+    //item is deleted from the scene
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         
