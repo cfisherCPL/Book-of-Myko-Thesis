7cfd8e89b555fa30b30ee6d2d9c7ca239dceae2b
Charles Fisher <48536902+cfisherCPL@users.noreply.github.com>
2024-12-01 14:02:50 -0600

    Transparent Treees
    Trees now go transparent when behind them.

---
+++ Assets/Scripts/TreeObject.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeObject : MonoBehaviour
+{
+    public SpriteRenderer mySpriteRenderer { get; set; }
+    private Color defaultColor;
+    private Color fadedColor;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+        defaultColor = mySpriteRenderer.color;
+        fadedColor = defaultColor;
+        fadedColor.a = 0.6f;
+    }
+
+    public void FadeOut()
+    {
+        mySpriteRenderer.color = fadedColor;
+    }
+
+    public void FadeIn()
+    { 
+        mySpriteRenderer.color = defaultColor;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            FadeOut();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            FadeIn();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
